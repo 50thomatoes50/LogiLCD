f5ffdf86e1afa686e1a1b08ec257f2ce24a00bb7
Thomas PORTASSAU <thomas.portassau@hotmail.fr>
2017-10-31 12:02:25 +0100

    added licence

--- LogiLCD.cpp
+++ LogiLCD.cpp
@@ -1,5 +1,6 @@
 /********************************************************************************
 Copyright (C) 2014 William Pearson <adocilesloth@gmail.com>
+Copyright (C) 2017 Thomas Portassau <50thomatoes50>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
